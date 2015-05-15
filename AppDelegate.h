0ccda42d5fa23169041e28e2f697dff96c3b77fc
21451061 <491191749@qq.com>
2015-05-15 11:52:28 +0800

    add to app branch

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  手势解锁
+//
+//  Created by 王威 on 15/4/18.
+//  Copyright (c) 2015年 zju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
